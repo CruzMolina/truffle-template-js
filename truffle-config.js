38cb72f3b66d454596dfb0ce1b5eabb2ce6a36f6
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2020-05-18 11:33:59 -0700

    add initial js template

---
+++ truffle-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  // See <https://www.trufflesuite.com/docs/truffle/reference/configuration>
+  // for more about customizing your Truffle configuration!
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*" // Match any network id
+    },
+    develop: {
+      port: 8545
+    }
+  }
+};
